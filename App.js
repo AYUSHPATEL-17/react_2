e23d0c4c29ddd50e346cf08ab846ba974ed14ad6
AYUSHPATEL-17 <89293391+AYUSHPATEL-17@users.noreply.github.com>
2024-01-22 21:47:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Global.css';
+
+import Watch from './watch';
+
+
+
+
+
+const App = () => { 
+  return (
+    <>
+      <Watch />
+    </>
+  )
+}
+
+export default App
